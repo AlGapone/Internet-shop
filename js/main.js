e45a09a07beb9316e0e6ea698f7f5f230763db17
Yaroslaw Hapon <rlowboy@gmail.com>
2015-04-07 18:53:56 +0300

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    positionCenter($(".write-us"));
+    /*открытие окна*/
+    $("#write-us").on("click", function(){
+        $(".write-us").fadeIn(300);
+    });
+    /*закрытие окна*/
+    $("#close-pop-up").on("click", function(){
+        $(".write-us").fadeOut(300);
+    });
+
+    /*позиционирование окна*/
+    function positionCenter(elem) {
+        elem.css({
+            marginTop: "-"  + elem.height() / 2 + "px",
+            marginLeft: "-" + elem.width()  / 2 + "px",
+
+        });
+    }
+});
+
+$(function(){
+
+    $(".top-menu-icon").on("click", function(){
+        $(".account-menu").slideToggle(300);
+    });
+
+    $(".drop-down-menu").on("click", function(){
+        $("nav ul").slideToggle(300);
+    });
+
+
+});
